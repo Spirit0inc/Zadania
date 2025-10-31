e27a7a8d01ae8119010756eeaf0b47f317ab9f70
Podstrelov Slava <podstrel385@gmail.com>
2025-10-31 19:14:40 +0700

    Системки 1

---
+++ prak_3/prak_3/Class_main_or_page/avtoriz_ril.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace prak_3
+{
+    /// <summary>
+    /// Логика взаимодействия для avtoriz_ril.xaml
+    /// </summary>
+    public partial class avtoriz_ril : Page
+    {
+        public avtoriz_ril()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new avtoriz());
+        }
+
+        private void GuestButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new avtoriz());
+        }
+    }
+}
